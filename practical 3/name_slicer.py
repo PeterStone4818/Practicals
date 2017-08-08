fb5d9f2115557df4f9e46e9b6f0e1f411587ffa7
PeterStone4818 <peter.stone@my.jcu.edu.au>
2017-08-08 12:32:25 +1000

    Name slicer is now completed

--- practical 3/name_slicer.py
+++ practical 3/name_slicer.py
@@ -1,5 +1,15 @@
-name = input("please enter your name.")
-if len(name) < 1 :
-    print(input("Error, name cannot be blank."))
-sliced_name = name[::2]
-print (sliced_name)
+def main():
+
+    name = get_name()
+    sliced_name = name[::2]
+    print (sliced_name)
+
+
+def get_name():
+    name = input("please enter your name.")
+    if len(name) < 1:
+        print(input("Error, name cannot be blank."))
+    return name
+
+
+main()
