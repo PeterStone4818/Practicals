bd92f015c3d91db8bf0f928801c3e8c5c6fb618c
PeterStone4818 <peter.stone@my.jcu.edu.au>
2017-08-11 14:49:39 +1000

    Celsius to Fahrenheit.py was updated to contain functions

---
+++ practical 3/Celsius to Fahrenheit.py
@@ -0,0 +1,31 @@
+def main():
+    MENU = "C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ (for quit)"
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = celsius_to_fahrenheit(celsius)
+            print("Result: {:.2f} F".format(fahrenheit))
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = fahrenheit_to_celsius(celsius, fahrenheit)
+            print("Result: {:.2f} C".format(celsius))
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+
+main()
---
+++ practical 3/fixed_score.py
@@ -0,0 +1,12 @@
+Celsius to Fahrenheit.py__author__ = 'Peter'
+score = float(input("Enter score: "))
+if score < 0:
+    print("Invalid Score")
+elif score > 100:
+    print("Invalid score")
+elif score > 90:
+    print("Excellent")
+elif score >= 50:
+    print("Passable")
+else:
+    print("Bad")
